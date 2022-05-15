a0e2abe32e6b1cc7e428b3ac4dd6998915c1ee60
Raul Costa Junior <raulcostajunior@gmail.com>
2022-05-15 12:02:15 +0200

    Turned into a web api.

--- hello.go
+++ hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 	"runtime"
 )
 
 func main() {
-	fmt.Println("Hi, there!")
-	fmt.Println("OS:   ", runtime.GOOS)
-	fmt.Println("ARCH: ", runtime.GOARCH)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, "<h2>Hello, %q!</h2>", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "<p>platform: %q:%q", runtime.GOOS, runtime.GOARCH)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
